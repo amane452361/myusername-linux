626a196f50061a7148a27181dbb167f4cb32ba4f
austin thomas <pr4njn@gmail.com>
2025-03-04 10:58:41 +0530

    Create blacklist.c

---
+++ certs/blacklist.c
@@ -0,0 +1,16 @@
+def boost(number):
+    """
+    Boosts the input number by multiplying it by 2.
+    
+    Parameters:
+    number (int or float): The number to be boosted.
+    
+    Returns:
+    int or float: The boosted number.
+    """
+    boosted_number = number * 2
+    return boosted_number
+
+# Example usage:
+result = boost(5)
+print(result)  # Output: 10
